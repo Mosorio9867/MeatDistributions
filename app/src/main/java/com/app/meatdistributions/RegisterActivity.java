09f180d8708e0f43c553aa7ca9e72bbd22394b01
Manuel Osorio <manuel.osorio@intelligenia.com>
2020-06-03 20:25:28 -0500

    register

--- app/src/main/java/com/app/meatdistributions/RegisterActivity.java
+++ app/src/main/java/com/app/meatdistributions/RegisterActivity.java
@@ -1,20 +1,62 @@
 package com.app.meatdistributions;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static class User {
+        public String id;
+        public String name;
+        public String email;
+
+        public User(String id, String name, String email) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+        }
+    }
+
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextConfirmPassword;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        findViewById(R.id.textLogin).setOnClickListener(this);
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
 
+        findViewById(R.id.textLogin).setOnClickListener(this);
+        findViewById(R.id.buttomregister).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
     }
 
     @Override
@@ -25,6 +67,87 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             case R.id.textLogin:
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
                 break;
+            case R.id.buttomregister:
+                register();
+                break;
         }
+    }
+
+    private void register() {
+        if (validForm()) {
+            String email = editTextEmail.getText().toString();
+            String password = editTextPassword.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                                updateUser();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Hubo un problema al registrar el usuario, intente mas tarde", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private boolean validForm() {
+        String name = editTextName.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String confirmPassword = editTextConfirmPassword.getText().toString();
+
+        if (name.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "El campo nombre es requerido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (email.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "El campo correo electrónico es requerido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "El campo contraseña es requerido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (confirmPassword.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "El campo confirmar contraseña es requerido", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (password.length() < 7) {
+            Toast.makeText(RegisterActivity.this, "Las contraseña debe tener minimo 7 caracteres", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(RegisterActivity.this, "Las contraseñas deben ser iguales", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void updateUser() {
+        String name = editTextName.getText().toString();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .build();
+
+        user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Se ha registrado correctamente", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                }
+            }
+        });
     }
 }
